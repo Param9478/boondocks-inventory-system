03a7ff83f72fac028e4d3815ba8a225c9546b1d1
param9478 <singh.parminder9478@gmail.com>
2026-02-16 02:19:28 -0700

    pwa

--- inventory-frontend/src/main.jsx
+++ inventory-frontend/src/main.jsx
@@ -2,5 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './App.css';
+import { registerSW } from 'virtual:pwa-register';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('New content available. Reload?')) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log('App is ready to work offline!');
+  },
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
